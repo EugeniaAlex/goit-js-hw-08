1ba89d5760962855ba696c433221d458408f7862
EugeniaAlex <eugenia.aleksandrova@gmail.com>
2022-01-18 16:17:39 +0200

    add-js-code-from#7
    Добавляет код из работы №7

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,35 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const divGaleryEl = document.querySelector('.gallery');
+
+function makeGaleryElementMarkup(item) {
+    
+    return `
+  <a class="gallery__item" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>`;
+};
+
+const makeGaleryElements = galleryItems.map(makeGaleryElementMarkup).join('');
+
+divGaleryEl.insertAdjacentHTML('beforeend', makeGaleryElements);
+
+
+divGaleryEl.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+    
+    event.preventDefault();
+    
+//     let gallery = new SimpleLightbox('.gallery a');
+// gallery.on('show.simplelightbox', function () {
+//     captionsData: `img`; captionDelay: `250`; 
+// });
+
+};
